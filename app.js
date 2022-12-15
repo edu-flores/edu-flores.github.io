52320456449ca06833c2c76e10ea117745a59df5
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-15 12:00:58 -0600

    Add shadow to header when scrolling

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+const header = document.querySelector(".header");
+
+// Add shadow to header after scrolling
+window.addEventListener("scroll", () => {
+  let scroll =  document.documentElement.scrollTop;
+  (scroll > 0) ? header.classList.add("shadow") : header.classList.remove("shadow");
+});
+
+// Open and close nav
 function toggleMenu() {
-  let header = document.querySelector(".header");
-  header.className = (header.className == "header") ? "header mobile-open" : "header";
+  if (!header.classList.contains("mobile-open")) {
+    header.classList.add("mobile-open");
+    header.classList.add("shadow");
+  } else {
+    header.classList.remove("mobile-open");
+    if (document.documentElement.scrollTop == 0) {
+      header.classList.remove("shadow");
+    }
+  }
 }
