79f3e4ebea4b0dd5146f79b3a8320839b802ad1b
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-15 14:07:05 -0600

    Highlight current section in navbar

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const header = document.querySelector(".header");
 
 // Add shadow to header after scrolling
 window.addEventListener("scroll", () => {
-  let scroll =  document.documentElement.scrollTop;
-  (scroll > 0) ? header.classList.add("shadow") : header.classList.remove("shadow");
+  (scrollY > 0) ? header.classList.add("shadow") : header.classList.remove("shadow");
 });
 
 // Open and close nav
@@ -15,3 +14,26 @@ function toggleMenu() {
     header.classList.remove("mobile-open");
   }
 }
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar--link");
+
+// Highlight current section in nav
+window.addEventListener("scroll", () => {
+  
+  let current;
+  sections.forEach(section => {
+    const sectionPos = section.offsetTop;
+    if (scrollY >= sectionPos) {
+      current = section.getAttribute("id");
+      return;
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") == "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
